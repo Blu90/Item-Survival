9c73fa0142338ba64cb881391a050fe50a74a61f
Joshua Devine <joshuaddevine@gmail.com>
2021-04-08 01:08:04 -0400

    Fancy new water baybeeeeee

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,74 +6,64 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 	
+	public GameObject player;
+	public GameObject surfaceWater;
+	public GameObject deepWater;
+
 	public float speed = 12f;
 	public float gravity = -9.81f;
-	public float buoyancy = -0.981f;
-	public float deepBuoyancy = 9.81f;
 	public float jumpHeight = 3f;
-	
-	public Transform groundCheck;
-	public Transform waterCheck;
 	public float groundDistance = 0.4f;
+
+	public Transform groundCheck;
 	public LayerMask groundMask;
-	public LayerMask surfaceWaterMask;
-	public LayerMask deepWaterMask;
 	
-	Vector3 velocity;
-	bool isGrounded;
-	bool isSubmergedSurface;
-	bool isSubmergedDeep;
-	bool isSubmerged;
-	
+	Vector3 move, velocity;
+
+	void Start()
+	{
+		player = GameObject.Find("First Person Player");
+		surfaceWater = GameObject.Find("SurfaceWater");
+		deepWater = GameObject.Find("DeepWater");
+	}
 
     // Update is called once per frame
     void Update()
     {
-		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-		isSubmergedSurface = Physics.CheckSphere(waterCheck.position, groundDistance, surfaceWaterMask);
-		isSubmergedDeep = Physics.CheckSphere(waterCheck.position, groundDistance, deepWaterMask);
 		
+		bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+		
+        float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+		
+		move = transform.right * x + transform.forward * z;
+		controller.Move(move * speed * Time.deltaTime);
+		
+		if(Input.GetButtonDown("Jump") && isGrounded)
+		{
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+		}
+		else if(Input.GetButton("Jump"))
+		{
+			if(player.transform.position.y < deepWater.transform.position.y)
+			{
+				velocity.y = Mathf.Sqrt(jumpHeight * -10f * gravity);
+			}
+			else if(player.transform.position.y < surfaceWater.transform.position.y)
+			{
+				velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+			}
+		}
 
-		
+		// update velocity relative to gravity
+		velocity.y += gravity * Time.deltaTime;
 		
 		// Caps downward velocity when on ground.
 		if(isGrounded && velocity.y <0)
 		{
 			velocity.y = -2f;
 		}
-		
-        float x = Input.GetAxis("Horizontal");
-		float z = Input.GetAxis("Vertical");
-		
-		Vector3 move = transform.right * x + transform.forward * z;
-		
-		controller.Move(move * speed * Time.deltaTime);
-		
-		if(Input.GetButtonDown("Jump"))
-		{
-			if (isSubmergedDeep || isSubmergedSurface)
-            {
-				velocity.y = Mathf.Sqrt(jumpHeight * 2f * buoyancy);
-			}
-			else if (isGrounded)
-			{
-				velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-			}
-		}
-		
-		velocity.y += gravity * Time.deltaTime;
-		
-		
-		if(isSubmergedSurface)
-		{
-			velocity.y = 1f;
-		}
-		
-		if(isSubmergedDeep)
-		{
-			velocity.y = 10f;
-		}
-		
+
 		controller.Move(velocity * Time.deltaTime);
 
     }
